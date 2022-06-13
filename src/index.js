a3b5cab15b62b4c6a1ecb01a5baeec44271068a2
brian ambundo <brian.katiechi9@gmail.com>
2022-06-13 15:17:51 +0300

    Done with the assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('#create-task-form').addEventListener('submit',function(e){
+    e.preventDefault()
+    const item = document.getElementById('new-task-description').value
+    console.log(item)
+    const text = document.createTextNode(item)
+    buildToDo(item)
+  })
+  
 });
+
+function buildToDo(todo){
+  let list = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click',handleDelete)
+  btn.textContent = 'Delete'
+  list.textContent = todo
+  list.appendChild(btn)
+  document.querySelector('ul').appendChild(list)
+}
+
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+
